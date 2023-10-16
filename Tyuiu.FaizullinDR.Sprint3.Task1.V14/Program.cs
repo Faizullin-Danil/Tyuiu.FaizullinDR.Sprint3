26c12659fe4767e1b3048d3f5017519832f85b03
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-10-16 17:21:18 +0500

    Task1

--- Tyuiu.FaizullinDR.Sprint3.Task1.V14/Program.cs
+++ Tyuiu.FaizullinDR.Sprint3.Task1.V14/Program.cs
@@ -25,10 +25,10 @@ namespace Tyuiu.FaizullinDR.Sprint3.Task1.V14
             Console.WriteLine("* Выполнил: Файзуллин Данил Рамилевич | ИИПБ-23-3                                      *");
             Console.WriteLine("****************************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                             *");
-            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда по формуле,      *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда по формуле,    *");
             Console.WriteLine("* при t = 0,7                                                                          *");
             Console.WriteLine("*     14                                                                               *");
-            Console.WriteLine("* S = ∑(t^k+1/(k+1))*cos(t)                                                                       *");
+            Console.WriteLine("* S = ∑(t^k+1/(k+1))*cos(t)                                                            *");
             Console.WriteLine("*     i=1                                                                              *");
             Console.WriteLine("****************************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
